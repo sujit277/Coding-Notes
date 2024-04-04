3cf9f7f0f69962cb5d85f2c028e2b06dae81ba89
Sujitkumar Verma <sujitkumar_verma@epam.com>
2024-04-04 20:58:04 +0530

    5 April 2024

--- Microservices/QuizService/src/main/java/com/quiz/entities/Question.java
+++ Microservices/QuizService/src/main/java/com/quiz/entities/Question.java
@@ -9,18 +9,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Quiz {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    transient private List<Question> questions;
+public class Question {
+    private  Long questionId;
+    private  String question;
+    private  Long quizId;
 }
--- Microservices/QuizService/src/main/java/com/quiz/services/QuizServiceImpl.java
+++ Microservices/QuizService/src/main/java/com/quiz/services/QuizServiceImpl.java
@@ -5,14 +5,18 @@ import com.quiz.repositories.QuizRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class QuizServiceImpl implements QuizService{
 
     private QuizRepository quizRepository;
 
-    public QuizServiceImpl(QuizRepository quizRepository) {
+    private QuestionClient questionClient;
+
+    public QuizServiceImpl(QuizRepository quizRepository, QuestionClient questionClient) {
         this.quizRepository = quizRepository;
+        this.questionClient = questionClient;
     }
 
     @Override
@@ -20,15 +24,23 @@ public class QuizServiceImpl implements QuizService{
         return quizRepository.save(quiz);
     }
 
+
+
     @Override
     public List<Quiz> get() {
-        return quizRepository.findAll();
+        List<Quiz> quizzes = quizRepository.findAll();
+        List<Quiz> newQuizList = quizzes.stream().map(quiz -> {
+            quiz.setQuestions(questionClient.getQuestionOfQuiz(quiz.getId()));
+            return quiz;
+        }).collect(Collectors.toList());
+        return newQuizList;
     }
 
     @Override
     public Quiz get(Long id) {
-        return quizRepository.findById(id).orElseThrow(()-> new RuntimeException("Quiz not found"));
+        Quiz quiz = quizRepository.findById(id).orElseThrow(()-> new RuntimeException("Quiz not found"));
+        quiz.setQuestions(questionClient.getQuestionOfQuiz(quiz.getId()));
+        return quiz;
     }
-
 
 }
